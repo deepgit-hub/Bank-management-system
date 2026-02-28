adbf140e2b60485f04038621462381c22b59aa4c
Deepak <darkdeepak001@gmail.com>
2026-02-28 22:54:47 +0530

    Update BankingManagementSystem.java

--- BankingManagementSystem.java
+++ BankingManagementSystem.java
@@ -17,23 +17,48 @@ class Account()
 }
 class SavingAccount
 {
-
+    void deposit()
+    {
+        System.out.print("How Much you have to deposit");
+        int deposit = scan.nextInt();
+        balance +=deposit;
+        System.out.print("Balance After Deposit "+balance);
+    }
 }
 class CurrentAccount
 {
 
 }
-class BankingManagementSystem
+class Practice
 {
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
+        SavingAccount s = new SavingAccount();
+        CurrentAccount c = new CurrentAccount();
         System.out.println("1.Saving Account ");
         System.out.println("2.Current Account ");
         System.out.println("Enter Account type ");
         int type = scan.nextInt();
         if(type==1)
         {
+            boolean run =true;
+            while(run)
+            {
+                System.out.println("1. Deposit");
+                System.out.println("2. Withdraw");
+                System.out.println("3. Check Balance");
+                System.out.println("4. Display Details");
+                System.out.println("5. Exit");
+                System.out.print("Enter your choice (1-5): ");
+                int choice=scan.nextInt();
+                switch(choice)
+                {
+                    case 1:
+                        
+                }
+            }
+            
             
             //Savings account
         }
